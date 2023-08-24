3ff5fa16b0eb573c85b3a81317dc3bc183ab7db8
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-08-24 14:16:45 +0300

    add npm sass, create component MyPageRecipes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import MainPageTitle from './shared/MyPageTitle/MyPageTitle';
+
+import '../styles/styles.scss';
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101',
+    // }}
     >
-      React homework template
+      <MainPageTitle text="My recipes" />
     </div>
   );
 };
