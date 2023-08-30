ac0cffef9c03b1b734c9268bd77c4e02e845e0f2
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-08-30 16:06:46 +0300

    commit

--- src/components/shared/api/MyRecipes/MyRecipes.js
+++ src/components/shared/api/MyRecipes/MyRecipes.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
 const recipesInstance = axios.create({
-  baseURL: ' http://project05-backend.onrender.com/api/own',
+  baseURL: ' https://project05-backend.onrender.com/api/own',
 });
 
+recipesInstance.defaults.headers.common['Authorization'] =
+  'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZWM2M2IxYTE5ZDcyYjdmM2YwYmE3MiIsImlhdCI6MTY5MzIxMzc2MywiZXhwIjoxNjk1MDI4MTYzfQ.CwsOFyvGlTrD4tZO7ikWXIdXwNWY8bpnu7VOisfE1_Q';
 export const getMyRecipes = async () => {
-  const { data } = await recipesInstance.get();
-  return data;
+  const { data: result } = await recipesInstance.get();
+
+  return result;
 };
 
 export const deleteRecipes = async id => {
