42206ef323982359798d3ae9679e8887169befea
Yaroslava2022 <114984565+Yaroslava2022@users.noreply.github.com>
2023-09-01 15:27:21 +0300

    стилі

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,4 +1,4 @@
-
+import { Link} from "react-router-dom";
 import css from './welcome-page.module.scss';
 const Welcome = ()=> {
     return(
@@ -9,15 +9,17 @@ const Welcome = ()=> {
                 <h1 className={css.welcome_title}>Welcome to the app!</h1>
                 <p className={css.welcome_text}>This app offers more than just a collection of recipes - it is designed to be your very own digital cookbook. You can easily save and retrieve your own recipes at any time.</p>
             <div className={css.button_form}>
-          
-                <button className={css.registration_button}>         
+            <Link to={`/registration`} >
+                <button className={css.registration_button} type="button">         
              Registration
             </button>
-          
-                <button className={css.signin_button}>
+            </Link>
+            <Link to={`/signin`} >
+                <button className={css.signin_button} type="button">
                   
             Sign in
             </button>
+            </Link>
             </div>
             </div>
             
