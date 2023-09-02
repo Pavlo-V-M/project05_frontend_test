5b2039a533ba9d775034f8fd88df72c3ca197ede
Pavlo-V-M <114091610+Pavlo-V-M@users.noreply.github.com>
2023-09-02 12:57:11 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import { authReducer } from './slices/auth';
-import { recipesReducer } from './recipes/recipes-slice';
+import { authReducer } from './slices/auth';
+
+// import { recipesReducer } from './recipes/recipes-slice'; <=== Від Юлі
+// Від Юлі ===> const store = configureStore({
+//   reducer: recipesReducer,
+// });
+// export default store; <==== Від Юлі
 
 const store = configureStore({
-  reducer: recipesReducer,
+    reducer: {
+        auth: authReducer
+    },
 });
 
 export default store;
