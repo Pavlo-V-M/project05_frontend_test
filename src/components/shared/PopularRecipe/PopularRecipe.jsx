d8e9eaf17542381b540ebb8b8b8c01bc77f69e74
Phokoos <hulkovskij@gmail.com>
2023-09-03 09:24:26 +0300

    Started code adding

---
+++ src/components/shared/PopularRecipe/PopularRecipe.jsx
@@ -0,0 +1,13 @@
+import styles from './PopularRecipe.module.scss';
+
+const PopularRecipes = () => {
+  return (
+    <>
+      <div className="container">
+        <h3 className={styles.popularTitle}>Popular recipe</h3>
+      </div>
+    </>
+  );
+};
+
+export default PopularRecipes;
