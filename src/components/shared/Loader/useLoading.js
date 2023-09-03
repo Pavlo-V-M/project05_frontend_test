730737107cea201693d097a1984c0ffd92c48399
Maksym Feshchenkko <MaksymFD@gmail.com>
2023-09-03 14:23:56 +0300

    error console

--- src/components/shared/Loader/useLoading.js
+++ src/components/shared/Loader/useLoading.js
@@ -3,19 +3,20 @@ import  { useState, useEffect } from 'react';
 const useLoading = () => {
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => { 
-    fetch('https://project05-backend.onrender.com/api-docs')
-      .then((response) => response.json())
-      .then((data) => {
-        const isDataLoaded = data && data.length > 0;
+useEffect(() => { 
+  fetch('https://project05-backend.onrender.com/api-docs')
+    .then((response) => response.json())
+    .then((data) => {
+      const isDataLoaded = data && data.length > 0;
 
-        setIsLoading(!isDataLoaded);
-      })
-      .catch((error) => {
-        console.error('Error loading data:', error);
-        setIsLoading(false); 
-      });
-  }, []);
+      setIsLoading(!isDataLoaded);
+    })
+    .catch((error) => {
+      // console.error('Error loading data:', error);
+      setIsLoading(false); 
+    });
+}, []);
+
 
 //   const timer = setTimeout(() => {
 //     setIsLoading(false);
