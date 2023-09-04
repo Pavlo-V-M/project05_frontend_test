c2bf0dc89387f6d0853dbd38d7c0fcebc020f9d7
Pavlo-V-M <114091610+Pavlo-V-M@users.noreply.github.com>
2023-09-04 17:30:34 +0300

    Update getIngredients.js

--- src/components/shared/DrinksPage/Api/getIngredients.js
+++ src/components/shared/DrinksPage/Api/getIngredients.js
@@ -3,16 +3,15 @@ import axios from 'axios';
 // const API_KEY = 'lavP2ue6A9';
 
 export const fetchIngredients = async () => {
-	const token =
-		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZWJlMjY0ZDJhZTdkYWJjYjViZTBkYiIsImlhdCI6MTY5MzUyMDI3MiwiZXhwIjoxNjk1MzM0NjcyfQ.6OCZSjvRR58UwD2zQ-ncGfxLwmeZBOzLdHFy4yx-4l0';
+  const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZWM2M2IxYTE5ZDcyYjdmM2YwYmE3MiIsImlhdCI6MTY5MzY3MjUwNCwiZXhwIjoxNjk1NDg2OTA0fQ.9OA4SHcrkeNE8YSWBxu60VqsGSpmmD3qJi6zQp-pwSE';
+  const instance = axios.create({
+    baseURL: 'https://project05-backend.onrender.com',
+  });
 
-	const instance = axios.create({
-		baseURL: 'https://project05-backend.onrender.com',
-	});
+  instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
-	instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  const { data } = await instance.get(`/api/ingredients/list`);
 
-	const { data } = await instance.get(`/api/ingredients/list`);
-
-	return data;
+  return data;
 };
