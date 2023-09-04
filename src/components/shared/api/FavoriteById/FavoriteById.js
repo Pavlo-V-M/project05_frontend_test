b06e5ca5cda1c68618749ad88184d68d6092a9ef
Myra77 <112496647+Myra77@users.noreply.github.com>
2023-09-04 16:24:09 +0200

    favorites-test1.1

---
+++ src/components/shared/api/FavoriteById/FavoriteById.js
@@ -0,0 +1,9 @@
+// import axios from "axios";				
+				
+// const token = localStorage.getItem('token');				
+						
+// export const instance = axios.create({				
+// baseURL: 'https://project05-backend.onrender.com/api'				
+// })				
+				
+// instance.defaults.headers.common['Authorization'] = `Bearer ${token}`				
