8cee59714852074472a292dfe7a9977cd2c0b4df
Pavlo-V-M <114091610+Pavlo-V-M@users.noreply.github.com>
2023-09-04 17:23:22 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,26 @@ import { Route, Routes } from 'react-router-dom';
 import { SharedLayout } from './components/SharedLayout/SharedLayout';
 import AuthForm from 'components/RegistrationPage/AuthForm';
 
+import useLoading from './components/shared/Loader/useLoading';
+import Loader from "./components/shared/Loader/Loader"
+
 // import BackgroundComponent from '../src/components/shared/Background/Background';
 
 const WelcomePage = lazy(() => import('./pages/WelcomePage/WelcomePage'));
 
-const MainPage = lazy(() => import('./pages/MainPage/MainPage'));
-const DrinksPage = lazy(() => import('./pages/DrinksPage/DrinksPage'));
-const AddRecipePage = lazy(() => import('./pages/AddRecipePage/AddRecipePage'));
-const MyRecipesPage = lazy(() => import('./pages/MyRecipesPage/MyRecipesPage'));
-const FavoritesPage = lazy(() => import('./pages/FavoritesPage/FavoritesPage'));
-const RecipePageById = lazy(() => import('./pages/RecipePage/RecipePage'));
+const MainPage = lazy (() => import('./pages/MainPage/MainPage'));
+const DrinksPage = lazy (() => import('./pages/DrinksPage/DrinksPage'));
+const AddRecipePage = lazy (() => import('./pages/AddRecipePage/AddRecipePage'));
+const MyRecipesPage = lazy (() => import('./pages/MyRecipesPage/MyRecipesPage'));
+const FavoritesPage = lazy (() => import('./pages/FavoritesPage/FavoritesPage'));
+const RecipePageById = lazy(()=> import ('./pages/RecipePage/RecipePage')) ;
 
 export const App = () => {
+  const { isLoading } = useLoading();
+
+  if (isLoading) {
+    return <Loader />;
+  }
   return (
     <Routes>
       <Route index element={<WelcomePage />} />
@@ -27,10 +35,10 @@ export const App = () => {
         <Route path="drinks" element={<DrinksPage />} />
         <Route path="add" element={<AddRecipePage />} />
         <Route path="my" element={<MyRecipesPage />} />
-        <Route path="favorite" element={<FavoritesPage />} />
-        <Route path="recipe/:recipeId" element={<RecipePageById />} />
+        <Route path="favorite" element={<FavoritesPage />}/>
+        <Route path="recipe/:recipeId" element={<RecipePageById />} />     
       </Route>
-      <Route path="*" element={'NotFound'}></Route>
+      <Route path="*" element={"NotFound"} ></Route>
     </Routes>
   );
 };
