bc8a1f4aa2ba539682751a2729008017c14bf01a
Yaroslava2022 <114984565+Yaroslava2022@users.noreply.github.com>
2023-09-07 18:04:00 +0300

    фух

--- src/pages/AddRecipePage/AddRecipePage.jsx
+++ src/pages/AddRecipePage/AddRecipePage.jsx
@@ -2,14 +2,20 @@ import MainPageTitle from '../../components/shared/MyPageTitle/MyPageTitle';
 import RecipeDescriptionFields from '../../components/shared/AddRecipeForm/RecipeDescriptionFields/RecipeDescriptionFields';
 import PopularRecipes from '../../components/shared/PopularRecipe/PopularRecipe';
 import FollowUsAdd from '../../components/shared/FollowUsAdd/FollowUsAdd';
-
+import styles from './AddRecipePage.module.scss';
 const AddRecipePage = () => {
   return (
-    <div className="container">
+    <div className="container" >
+      <div className={styles.container}>
+      <div className={styles.left}>
       <MainPageTitle text="Add recipe" />
       <RecipeDescriptionFields />
+      </div>
+      <div className={styles.right}>
       <FollowUsAdd />
       <PopularRecipes />
+      </div>
+      </div>
     </div>
   );
 };
